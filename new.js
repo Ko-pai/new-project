bbdb555d5881197eb59574064aec00cf6104a12a
Pai thet <leontinus866@gmail.com>
2023-03-20 18:58:02 +0630

    Final Result Commit

--- new.js
+++ new.js
@@ -1,1 +1,25 @@
-const abc= document.querySelector(".a")
+let abc =document.querySelectorAll("#a")
+
+
+abc.forEach((e)=>{
+    // const {reaction,memory,verbal,visual} = e
+
+    
+    e.addEventListener("click" ,()=>{
+        
+        if(e.classList.contains("reactionColor")){
+            e.classList.remove("reactionColor")
+        }else{
+            e.classList.add("reactionColor")
+        }
+        
+    })
+
+})
+// reaction.addEventListener("click",()=>{
+//     
+// })
+
+
+
+
